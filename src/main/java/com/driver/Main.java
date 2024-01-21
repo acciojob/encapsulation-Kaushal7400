9a5eba30b22dbbcc0768d4b82077f96c235c21e6
Kaushalendra singh <ksb250998@gmail.com>
2024-01-21 16:02:23 +0530

    Encapsulation done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly myObj = new RWOnly();
+        myObj.setName("Kaushal"); // Set the value of the name variable to "Kaushal"
+        System.out.println(myObj.getName());
+
+    }
+
 }
